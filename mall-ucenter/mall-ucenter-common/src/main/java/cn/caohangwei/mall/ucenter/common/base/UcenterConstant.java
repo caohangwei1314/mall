b5bc8db18c4c6b72c996ea6af3736d6553922322
PinuoC <104079381@qq.com>
2019-09-09 16:22:20 +0800

    add custom class

---
+++ mall-ucenter/mall-ucenter-common/src/main/java/cn/caohangwei/mall/ucenter/common/base/UcenterConstant.java
@@ -0,0 +1,7 @@
+package cn.caohangwei.mall.ucenter.common.base;
+
+public class UcenterConstant {
+
+    public static final String COOKIE_TOKEN_NAME = "ucenter-token";
+
+}
---
+++ mall-ucenter/mall-ucenter-common/src/main/java/cn/caohangwei/mall/ucenter/common/base/UcenterUserLoginInfo.java
@@ -0,0 +1,65 @@
+package cn.caohangwei.mall.ucenter.common.base;
+
+import java.io.Serializable;
+
+public class UcenterUserLoginInfo implements Serializable {
+
+    private String phone;
+
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", password=").append(password);
+        sb.append(", phone=").append(phone);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        UcenterUserLoginInfo other = (UcenterUserLoginInfo) that;
+        return (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
+                && (this.getPhone() == null ? other.getPhone() == null : this.getPhone().equals(other.getPhone()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        result = prime * result + ((getPhone() == null) ? 0 : getPhone().hashCode());
+        return result;
+    }
+}
