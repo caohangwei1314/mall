c970384099f292336585d9bfb3f78d512b36aa1d
PinuoC <104079381@qq.com>
2019-09-13 17:00:16 +0800

    add BaseResult.java Serializable

--- mall-common/src/main/java/cn/caohangwei/mall/common/base/BaseResult.java
+++ mall-common/src/main/java/cn/caohangwei/mall/common/base/BaseResult.java
@@ -1,15 +1,19 @@
 package cn.caohangwei.mall.common.base;
 
+import java.io.Serializable;
+
 /**
  * Partition returns result class.
  */
-public class BaseResult {
+public class BaseResult implements Serializable {
 
-    private int code;
+    private Integer code;
 
     private String msg;
 
     private Object data;
+
+    private static final long serialVersionUID = 1L;
 
     public BaseResult(int code,String msg,Object data){
         this.code = code;
@@ -23,11 +27,11 @@ public class BaseResult {
         this.data = data;
     }
 
-    public int getCode() {
+    public Integer getCode() {
         return code;
     }
 
-    public void setCode(int code) {
+    public void setCode(Integer code) {
         this.code = code;
     }
 
@@ -45,5 +49,45 @@ public class BaseResult {
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", code=").append(code);
+        sb.append(", msg=").append(msg);
+        sb.append(", data=").append(data);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        BaseResult other = (BaseResult) that;
+        return (this.getCode() == null ? other.getCode() == null : this.getCode().equals(other.getCode()))
+                && (this.getMsg() == null ? other.getMsg() == null : this.getMsg().equals(other.getMsg()))
+                && (this.getData() == null ? other.getData() == null : this.getData().equals(other.getData()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getCode() == null) ? 0 : getCode().hashCode());
+        result = prime * result + ((getMsg() == null) ? 0 : getMsg().hashCode());
+        result = prime * result + ((getData() == null) ? 0 : getData().hashCode());
+        return result;
     }
 }
